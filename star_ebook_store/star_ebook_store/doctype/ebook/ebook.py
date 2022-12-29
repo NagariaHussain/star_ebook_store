598b4afc03185ff49b25b61fcddc746934863c17
Hussain Nagaria <hussainbhaitech@gmail.com>
2022-12-29 22:58:11 +0530

    feat: enable web view for ebook

--- star_ebook_store/star_ebook_store/doctype/ebook/ebook.py
+++ star_ebook_store/star_ebook_store/doctype/ebook/ebook.py
@@ -2,7 +2,7 @@
 # For license information, please see license.txt
 
 # import frappe
-from frappe.model.document import Document
+from frappe.website.website_generator import WebsiteGenerator
 
-class eBook(Document):
+class eBook(WebsiteGenerator):
 	pass
