7df32d647fc16a963edf2f087268f6da39f1ad5f
Hussain Nagaria <hussainbhaitech@gmail.com>
2022-12-31 00:22:10 +0530

    fix: generate route

--- star_ebook_store/star_ebook_store/doctype/ebook/ebook.py
+++ star_ebook_store/star_ebook_store/doctype/ebook/ebook.py
@@ -4,9 +4,14 @@
 import frappe
 
 from frappe.website.website_generator import WebsiteGenerator
+from frappe.website.utils import cleanup_page_name
 
 
 class eBook(WebsiteGenerator):
+	def validate(self):
+		if not self.route:
+			self.route = f"/store/{cleanup_page_name(self.name)}"
+
 	def get_context(self, context):
 		context.author = frappe.db.get_value(
 			"Author", self.author, ["full_name as name", "bio"], as_dict=True
