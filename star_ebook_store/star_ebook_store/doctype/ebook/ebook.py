216b7f48268b5ec53266bad612afa8f685cc532c
Hussain Nagaria <hussainbhaitech@gmail.com>
2022-12-30 15:59:12 +0530

    fix: alias full_name to name

--- star_ebook_store/star_ebook_store/doctype/ebook/ebook.py
+++ star_ebook_store/star_ebook_store/doctype/ebook/ebook.py
@@ -9,5 +9,5 @@ from frappe.website.website_generator import WebsiteGenerator
 class eBook(WebsiteGenerator):
 	def get_context(self, context):
 		context.author = frappe.db.get_value(
-			"Author", self.author, ["full_name", "bio"], as_dict=True
+			"Author", self.author, ["full_name as name", "bio"], as_dict=True
 		)
