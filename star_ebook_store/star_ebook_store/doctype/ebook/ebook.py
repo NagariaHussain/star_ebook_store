0687989ebd8eb2932470a38be45ff97556a9f964
Hussain Nagaria <hussainbhaitech@gmail.com>
2022-12-31 00:24:07 +0530

    fix: route

--- star_ebook_store/star_ebook_store/doctype/ebook/ebook.py
+++ star_ebook_store/star_ebook_store/doctype/ebook/ebook.py
@@ -10,7 +10,7 @@ from frappe.website.utils import cleanup_page_name
 class eBook(WebsiteGenerator):
 	def validate(self):
 		if not self.route:
-			self.route = f"/store/{cleanup_page_name(self.name)}"
+			self.route = f"store/{cleanup_page_name(self.name)}"
 
 	def get_context(self, context):
 		context.author = frappe.db.get_value(
