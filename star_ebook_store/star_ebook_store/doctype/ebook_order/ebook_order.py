46bcf08ae3214970ea6325c5c0c0f2957f325e8a
Hussain Nagaria <hussainbhaitech@gmail.com>
2023-01-14 12:41:23 +0530

    feat: deliver ebook on payment

--- star_ebook_store/star_ebook_store/doctype/ebook_order/ebook_order.py
+++ star_ebook_store/star_ebook_store/doctype/ebook_order/ebook_order.py
@@ -1,8 +1,20 @@
 # Copyright (c) 2023, Hussain Nagaria and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
+
 class eBookOrder(Document):
-	pass
+	def on_update(self):
+		if self.status == "Paid":
+			self.deliver_ebook()
+
+	def deliver_ebook(self):
+		ebook_doc = frappe.get_doc("eBook", self.ebook)
+
+		try:
+			ebook_doc.send_via_email(self.customer_email)
+			ebook_doc.status = "Delivered"
+		except:
+			ebook_doc.log_error("Ebook Deliver Error")
